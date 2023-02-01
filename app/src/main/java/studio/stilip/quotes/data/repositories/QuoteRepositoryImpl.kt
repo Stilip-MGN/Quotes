11a28b296979809a1e176c7b2c3fd948dd3e787a
Andrew Krysh <vip.krysh@mail.ru>
2023-02-01 12:59:52 +0500

    удалил не нужные фейковые данные

--- app/src/main/java/studio/stilip/quotes/data/repositories/QuoteRepositoryImpl.kt
+++ app/src/main/java/studio/stilip/quotes/data/repositories/QuoteRepositoryImpl.kt
@@ -11,36 +11,6 @@ class QuoteRepositoryImpl @Inject constructor(
     private val retrofitService: RetrofitServiceQuote
 ) : QuoteRepository {
 
-    val fake = listOf(
-        Quote(
-            1, "", 0, "Честно говоря, моя дорогая, мне наплевать", emptyList(), emptyList()
-        ),
-        Quote(
-            2, "2019-03-11", 1, "Я собираюсь сделать ему предложение, от которого он не сможет отказаться",
-            listOf(
-                "android", "iOS", "MacOS", "Ubuntu", "Centos", "Solaris", "Windows", "IpadOS"
-            ),
-            listOf(
-                "YELLOW", "CYAN", "LTGRAY"
-            )
-        ),
-        Quote(
-            1, "", 1, "Честно говоря, моя дорогая, мне наплевать", emptyList(), emptyList()
-        ),
-        Quote(
-            2, "", 0, "Я собираюсь сделать ему предложение, от которого он не сможет отказаться", emptyList(), emptyList()
-        ),
-        Quote(
-            1, "", 0, "Честно говоря, моя дорогая, мне наплевать", emptyList(), emptyList()
-        ),
-        Quote(
-            2, "", 0, "Я собираюсь сделать ему предложение, от которого он не сможет отказаться", emptyList(), emptyList()
-        ),
-        Quote(
-            1, "", 1, "Честно говоря, моя дорогая, мне наплевать", emptyList(), emptyList()
-        ),
-    )
-
     override fun getQuotesWithOffset(offset: Int): Single<List<Quote>> {
         return retrofitService.getQuotesWithOffset(offset.toString())
             .map { list -> list.map { q -> q.toDomain() } }
